ffcf88ce595109830e7829e9d3443d2b824f70aa
auvmacintosh <120029790@qq.com>
2018-09-08 06:44:41 +0800

    tried different configuration file location

--- src/main/java/info/natebowang/wbqly/WbqlyApplication.java
+++ src/main/java/info/natebowang/wbqly/WbqlyApplication.java
@@ -4,9 +4,7 @@ import info.natebowang.wbqly.qlyuser.QlyuserDetailsService;
 import info.natebowang.wbqly.qlyuser.QlyuserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -14,36 +12,39 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class WbqlyApplication extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    QlyuserRepository qlyuserRepository;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(new QlyuserDetailsService(qlyuserRepository)).
-                passwordEncoder(new BCryptPasswordEncoder());
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .httpBasic().and()
-                .authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/qlyusers").permitAll()
-                .antMatchers(HttpMethod.POST, "/qlyusers").permitAll().and()
-                .csrf().disable()
-                .rememberMe();
-    }
+public class WbqlyApplication {
 
     @Configuration
     @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
     public static class MethodSecurityConf extends GlobalMethodSecurityConfiguration {
+    }
+
+    @Configuration
+    @EnableWebSecurity
+    public static class WebSecurityConf extends WebSecurityConfigurerAdapter {
+        @Autowired
+        QlyuserRepository qlyuserRepository;
+
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.userDetailsService(new QlyuserDetailsService(qlyuserRepository)).
+                    passwordEncoder(new BCryptPasswordEncoder());
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .httpBasic().and()
+                    .authorizeRequests()
+                    .antMatchers(HttpMethod.GET, "/qlyusers").permitAll()
+                    .antMatchers(HttpMethod.POST, "/qlyusers").permitAll().and()
+                    .csrf().disable()
+                    .rememberMe();
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/info/natebowang/wbqly/qlyuser/QlyuserRepository.java
+++ src/main/java/info/natebowang/wbqly/qlyuser/QlyuserRepository.java
@@ -3,10 +3,14 @@ package info.natebowang.wbqly.qlyuser;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.annotation.Secured;
 
 @RepositoryRestResource
 public interface QlyuserRepository extends PagingAndSortingRepository<Qlyuser, Long> {
 
     Qlyuser findByUsername(@Param("username") String name);
 
+    @Override
+    @Secured("ROLE_ADMIN")
+    <S extends Qlyuser> S save(S s);
 }
