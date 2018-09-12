96250d77ad0a29f59ee425a5e321ad6fc3c56616
auvmacintosh <120029790@qq.com>
2018-09-12 18:25:24 +0800

    try

--- src/main/java/info/natebowang/wbqly/qlyuser/QlyuserController.java
+++ src/main/java/info/natebowang/wbqly/qlyuser/QlyuserController.java
@@ -1,17 +1,14 @@
 package info.natebowang.wbqly.qlyuser;
 
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class QlyuserController {
 
     @PreAuthorize("#username == authentication.name")
-    @RequestMapping(value = "/haha")
-    public String findByUsernameWithSe(@RequestParam("username") String username) {
+    @RequestMapping(value = "/haha/{username}")
+    public String findByUsernameWithSe(@PathVariable("username") String username) {
         return username;
     }
 
