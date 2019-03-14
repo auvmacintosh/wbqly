76c5e75b4c6cd2257daf7476dc49ebd4b0c1751b
auvmacintosh <auvmacintosh@qq.com>
2019-03-14 16:59:20 +0800

    把success handler和failure handler都自定义了，原来的302太土了

--- src/main/java/info/natebowang/wbqly/WbqlyApplication.java
+++ src/main/java/info/natebowang/wbqly/WbqlyApplication.java
@@ -14,11 +14,19 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Arrays;
 
 @SpringBootApplication
@@ -40,7 +48,22 @@ public class WbqlyApplication {
         protected void configure(HttpSecurity http) throws Exception {
             http.cors()
 //                    .and().httpBasic() // 可以直接运行，走HTTP协议
-                    .and().formLogin().defaultSuccessUrl("http://localhost:3000", true) //
+                    .and().formLogin().successHandler(new AuthenticationSuccessHandler() {
+                @Override
+                public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                                    Authentication authentication) throws IOException,
+                        ServletException {
+                    //do nothing 不然这里傻了吧唧给一个302跳转。
+                }
+
+            }).failureHandler(new AuthenticationFailureHandler() {
+                @Override
+                public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                                                    AuthenticationException exception) throws IOException,
+                        ServletException {
+                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Authentication Failed. Wrong username or password or both");
+                }
+            })
                     // 可以直接运行，这个不满足任何协议，只由程序控制，有logout机制
 //                    .and().oauth2Login() // 还得配置bean才能运行
                     .and().authorizeRequests()
@@ -55,7 +78,7 @@ public class WbqlyApplication {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
